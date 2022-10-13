2df5a1530eeffa9a1c1b9a0ef0c764e2c3e67812
Балкунов Владимир <balkunov@mzta.ru>
2022-10-13 12:33:50 +0300

    ui cipher

--- ui_mainwindow.h
+++ ui_mainwindow.h
@@ -35,18 +35,20 @@ public:
     QHBoxLayout *horizontalLayout;
     QHBoxLayout *centralLayout;
     QGridLayout *gridLayout_3;
-    QLabel *label_3;
-    QSpacerItem *verticalSpacer;
     QLineEdit *serialnum;
-    QPushButton *start;
     QCheckBox *flash;
-    QComboBox *module;
+    QSpacerItem *verticalSpacer;
     QLabel *label_2;
-    QLabel *label_5;
-    QComboBox *modification;
-    QLabel *label_4;
     QLineEdit *mac;
     QCheckBox *test;
+    QLabel *label_4;
+    QComboBox *modification;
+    QLabel *label_3;
+    QLabel *label;
+    QLabel *label_5;
+    QComboBox *module;
+    QPushButton *start;
+    QLineEdit *cipher;
     QVBoxLayout *verticalLayout;
     QHBoxLayout *codeEditorMenu;
     QCodeEditor *codeEditor;
@@ -66,46 +68,40 @@ public:
         centralLayout->setObjectName(QString::fromUtf8("centralLayout"));
         gridLayout_3 = new QGridLayout();
         gridLayout_3->setObjectName(QString::fromUtf8("gridLayout_3"));
-        label_3 = new QLabel(centralwidget);
-        label_3->setObjectName(QString::fromUtf8("label_3"));
-
-        gridLayout_3->addWidget(label_3, 2, 0, 1, 2);
-
-        verticalSpacer = new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding);
-
-        gridLayout_3->addItem(verticalSpacer, 8, 0, 1, 2);
-
         serialnum = new QLineEdit(centralwidget);
         serialnum->setObjectName(QString::fromUtf8("serialnum"));
 
-        gridLayout_3->addWidget(serialnum, 5, 0, 1, 2);
-
-        start = new QPushButton(centralwidget);
-        start->setObjectName(QString::fromUtf8("start"));
-        start->setMinimumSize(QSize(0, 20));
-
-        gridLayout_3->addWidget(start, 11, 0, 1, 2);
+        gridLayout_3->addWidget(serialnum, 7, 0, 1, 2);
 
         flash = new QCheckBox(centralwidget);
         flash->setObjectName(QString::fromUtf8("flash"));
 
-        gridLayout_3->addWidget(flash, 10, 0, 1, 2);
+        gridLayout_3->addWidget(flash, 12, 0, 1, 2);
 
-        module = new QComboBox(centralwidget);
-        module->addItem(QString());
-        module->setObjectName(QString::fromUtf8("module"));
+        verticalSpacer = new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding);
 
-        gridLayout_3->addWidget(module, 1, 0, 1, 2);
+        gridLayout_3->addItem(verticalSpacer, 10, 0, 1, 2);
 
         label_2 = new QLabel(centralwidget);
         label_2->setObjectName(QString::fromUtf8("label_2"));
 
         gridLayout_3->addWidget(label_2, 0, 0, 1, 2);
 
-        label_5 = new QLabel(centralwidget);
-        label_5->setObjectName(QString::fromUtf8("label_5"));
+        mac = new QLineEdit(centralwidget);
+        mac->setObjectName(QString::fromUtf8("mac"));
 
-        gridLayout_3->addWidget(label_5, 6, 0, 1, 2);
+        gridLayout_3->addWidget(mac, 9, 0, 1, 2);
+
+        test = new QCheckBox(centralwidget);
+        test->setObjectName(QString::fromUtf8("test"));
+        test->setChecked(true);
+
+        gridLayout_3->addWidget(test, 11, 0, 1, 2);
+
+        label_4 = new QLabel(centralwidget);
+        label_4->setObjectName(QString::fromUtf8("label_4"));
+
+        gridLayout_3->addWidget(label_4, 6, 0, 1, 2);
 
         modification = new QComboBox(centralwidget);
         modification->addItem(QString());
@@ -113,21 +109,38 @@ public:
 
         gridLayout_3->addWidget(modification, 3, 0, 1, 2);
 
-        label_4 = new QLabel(centralwidget);
-        label_4->setObjectName(QString::fromUtf8("label_4"));
+        label_3 = new QLabel(centralwidget);
+        label_3->setObjectName(QString::fromUtf8("label_3"));
 
-        gridLayout_3->addWidget(label_4, 4, 0, 1, 2);
+        gridLayout_3->addWidget(label_3, 2, 0, 1, 2);
 
-        mac = new QLineEdit(centralwidget);
-        mac->setObjectName(QString::fromUtf8("mac"));
+        label = new QLabel(centralwidget);
+        label->setObjectName(QString::fromUtf8("label"));
 
-        gridLayout_3->addWidget(mac, 7, 0, 1, 2);
+        gridLayout_3->addWidget(label, 4, 0, 1, 2);
 
-        test = new QCheckBox(centralwidget);
-        test->setObjectName(QString::fromUtf8("test"));
-        test->setChecked(true);
+        label_5 = new QLabel(centralwidget);
+        label_5->setObjectName(QString::fromUtf8("label_5"));
 
-        gridLayout_3->addWidget(test, 9, 0, 1, 2);
+        gridLayout_3->addWidget(label_5, 8, 0, 1, 2);
+
+        module = new QComboBox(centralwidget);
+        module->addItem(QString());
+        module->setObjectName(QString::fromUtf8("module"));
+
+        gridLayout_3->addWidget(module, 1, 0, 1, 2);
+
+        start = new QPushButton(centralwidget);
+        start->setObjectName(QString::fromUtf8("start"));
+        start->setMinimumSize(QSize(0, 20));
+
+        gridLayout_3->addWidget(start, 13, 0, 1, 2);
+
+        cipher = new QLineEdit(centralwidget);
+        cipher->setObjectName(QString::fromUtf8("cipher"));
+        cipher->setMaxLength(16);
+
+        gridLayout_3->addWidget(cipher, 5, 0, 1, 2);
 
 
         centralLayout->addLayout(gridLayout_3);
@@ -167,19 +180,21 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
-        label_3->setText(QCoreApplication::translate("MainWindow", "\320\234\320\276\320\264\320\270\321\204\320\270\320\272\320\260\321\206\320\270\321\217 (argv[2])", nullptr));
         serialnum->setText(QCoreApplication::translate("MainWindow", "0", nullptr));
-        start->setText(QCoreApplication::translate("MainWindow", "\320\227\320\260\320\277\321\203\321\201\320\272", nullptr));
         flash->setText(QCoreApplication::translate("MainWindow", "\320\237\321\200\320\276\321\210\320\270\320\262\320\272\320\260", nullptr));
-        module->setItemText(0, QCoreApplication::translate("MainWindow", "4 - kB-EG", nullptr));
-
         label_2->setText(QCoreApplication::translate("MainWindow", "\320\234\320\276\320\264\321\203\320\273\321\214 (argv[1])", nullptr));
-        label_5->setText(QCoreApplication::translate("MainWindow", "MAC-\320\260\320\264\321\200\320\265\321\201", nullptr));
-        modification->setItemText(0, QCoreApplication::translate("MainWindow", "4", nullptr));
-
-        label_4->setText(QCoreApplication::translate("MainWindow", "\320\241\320\265\321\200\320\270\320\271\320\275\321\213\320\271 \320\275\320\276\320\274\320\265\321\200", nullptr));
         mac->setText(QCoreApplication::translate("MainWindow", "00-FF-38-34-96-A5", nullptr));
         test->setText(QCoreApplication::translate("MainWindow", "\320\237\321\200\320\276\320\262\320\265\321\200\320\272\320\260", nullptr));
+        label_4->setText(QCoreApplication::translate("MainWindow", "\320\241\320\265\321\200\320\270\320\271\320\275\321\213\320\271 \320\275\320\276\320\274\320\265\321\200", nullptr));
+        modification->setItemText(0, QCoreApplication::translate("MainWindow", "4", nullptr));
+
+        label_3->setText(QCoreApplication::translate("MainWindow", "\320\234\320\276\320\264\320\270\321\204\320\270\320\272\320\260\321\206\320\270\321\217 (argv[2])", nullptr));
+        label->setText(QCoreApplication::translate("MainWindow", "\320\250\320\270\321\204\321\200", nullptr));
+        label_5->setText(QCoreApplication::translate("MainWindow", "MAC-\320\260\320\264\321\200\320\265\321\201", nullptr));
+        module->setItemText(0, QCoreApplication::translate("MainWindow", "4 - kB-EG", nullptr));
+
+        start->setText(QCoreApplication::translate("MainWindow", "\320\227\320\260\320\277\321\203\321\201\320\272", nullptr));
+        cipher->setText(QCoreApplication::translate("MainWindow", "4111150302111120", nullptr));
     } // retranslateUi
 
 };
